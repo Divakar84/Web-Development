93a6aa0ee3289173f3ab9be4938bab5b1a17eb97
Divakar Pal <divakarpal00011@gmail.com>
2025-02-07 14:50:33 +0530

    JS Variable, Data Types and Objects

---
+++ Day30/script.js
@@ -0,0 +1,44 @@
+console.log("Hello World");
+
+var a = 5;
+a = a + 1;
+var b = 6;
+var c = "JavaScript";
+ //var 55a = "Rohan";  // NOT ALLOWED
+
+//console.log(a + b + 8)
+//console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 66
+    console.log(a); // output is 66 
+}
+console.log(a); //Output is 6
+
+
+//const a1 = 6;
+//a1 = a1 +1;
+//NOT ALLOWED BECAUSE a1 IS CONSTANT
+
+
+let x = "JavaScript";
+let y = 33;
+let z = 4.44;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log( typeof  x,  typeof y,  typeof z,  typeof p,  typeof q,  typeof r);
+
+let object ={
+    name : "Rohan",
+    "Job Code": 9797,
+    is_handsome : true
+}
+
+console.log(object);
+object.salary = 6838;
+console.log(object);
+object.salary = "1 Crore";
+console.log(object);
