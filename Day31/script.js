277261a7ae47df8d3eb98fecf112df308500dfd9
Divakar Pal <divakarpal00011@gmail.com>
2025-02-07 20:42:47 +0530

    JavaScript Loops

---
+++ Day31/script.js
@@ -0,0 +1,44 @@
+console.log("Hello World:");
+console.log("Hello world");
+
+//let age = 0;
+
+/*if (age>18){
+    console.log("You can drive.")
+}
+
+else{
+    console.log("You cannot drive.")
+}*/
+
+/*let grace = 2;
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age % grace)
+console.log(age ** grace)
+
+if((age+grace)>18){
+    console.log("You can drive");
+}
+
+else{
+    console.log("You cannot drive");
+}*/
+
+/*if(age>=18){
+    console.log("You can drive");
+}
+else if(age == 0){
+    console.log("Are you kidding?");
+}
+else{
+    console.log("You cannot drive");
+}*/
+
+a = 6;
+b = 8;
+let c = a > b ? (a-b) : (b-a);
+console.log(c);
+
