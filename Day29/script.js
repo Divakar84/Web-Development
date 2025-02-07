167a9c2814cac6d8ed14e4cef2e3e7ca4e212a83
Divakar Pal <divakarpal00011@gmail.com>
2025-02-07 13:56:17 +0530

    JavaScript Intro..

---
+++ Day29/script.js
@@ -0,0 +1,16 @@
+alert("Hello World");
+
+console.log("Code is running....")
+
+var a = prompt("enter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is Blasting....")
+}
+
+else{
+    console.log("Computer is not blasting.")
+}
+
+console.log("Your number is " + a)
