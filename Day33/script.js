51ad9ceab8bbf9c405c339d0bdf28e1b52a60e80
Divakar Pal <divakarpal00011@gmail.com>
2025-02-08 13:21:18 +0530

    JS Functions

---
+++ Day33/script.js
@@ -0,0 +1,34 @@
+function nice(name) {
+    console.log("Hey " + name + " you are nice!")
+    console.log("Hey " + name + " you are good!")
+    console.log("Hey " + name + " you are better!")
+    console.log("Hey " + name + " your tshirt is nice!")
+    console.log("Hey " + name + " you are nice!")
+}
+/*nice("Rohan");
+nice("Shivam");*/
+
+function sum(a, b) {
+   // console.log(a + b);
+   //console.log(a, b)
+    return a + b;
+    
+}
+
+//sum(8, 8);
+result1 = sum(3, 5)
+result2 = sum(5, 5)
+result3 = sum(3, 6)
+
+/*console.log("The sum of these numbers is :" , result1)
+console.log("The sum of these numbers is :" , result2)
+console.log("The sum of these numbers is :" , result3)*/
+
+const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+ 
+}
+
+func1(34);
+func1(65);
+func1(85);
